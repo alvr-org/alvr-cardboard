1562844bd1de7c0f322654d712aaa12ac9f014e0
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2022-10-29 15:56:12 +0200

    Remove ALVR submodule, download with script

--- prepare-deps.sh
+++ prepare-deps.sh
@@ -1,14 +1,27 @@
-# Download sdk source
-rm -r cardboard
-curl -sLS https://github.com/googlevr/cardboard/archive/v1.17.0.zip > download.zip
+SDK_VRS="1.17.0"
+
+# Download ALVR source
+rm -r "ALVR"
+curl -sLS "https://github.com/alvr-org/ALVR/archive/refs/heads/master.zip" > download.zip
 unzip download.zip
 rm download.zip
-mv ./cardboard-1.17.0 ./cardboard
+mv ALVR-master ALVR
+
+rm -r cardboard
+
+# Download sdk source
+rm -r "cardboard-${SDK_VRS}"
+curl -sLS "https://github.com/googlevr/cardboard/archive/v${SDK_VRS}.zip" > download.zip
+unzip download.zip
+rm download.zip
 
 # Build sdk
-cd ./cardboard
+pushd "cardboard-${SDK_VRS}"
 ./gradlew sdk:assembleRelease -Parm64-v8a
+popd
 
 # Prepare files
-mv ./sdk/build/outputs/aar/sdk-release.aar ./cardboard-sdk.aar
-cp ./sdk/include/cardboard.h ./cardboard.h
+mkdir cardboard
+mv "cardboard-${SDK_VRS}/sdk/build/outputs/aar/sdk-release.aar" cardboard/cardboard-sdk.aar
+cp "cardboard-${SDK_VRS}/sdk/include/cardboard.h" cardboard/cardboard.h
+rm -r "cardboard-${SDK_VRS}"
